04a528a1006f1ed67262506b183a8e8fd8bcf32c
Doddlebopper <122325330+Doddlebopper@users.noreply.github.com>
2023-03-09 17:54:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var count = 0;
 var btn = document.getElementById("btnCount");
 var disp = document.getElementById("display");
+var btn1 = document.getElementById("btnReset");
 
 btn.onclick = function () {
  count++;
   disp.innerHTML = count;
 }
-/*
-rstBtn.onclick = function () {
+
+btn1.onclick = function () {
  count = 0;
  disp.innerHTML = count;
 }
-*/
+
