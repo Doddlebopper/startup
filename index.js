f415daac1688b70e6299c5d87dc9a76dc44f861d
Doddlebopper <122325330+Doddlebopper@users.noreply.github.com>
2023-03-09 17:43:17 -0700

    add reset button function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var count = 0;
 var btn = document.getElementById("btnCount");
+var rstBtn = document.getElemenetById("btnReset");
 var disp = document.getElementById("display");
 
 btn.onclick = function () {
  count++;
   disp.innerHTML = count;
+}
+
+rstBtn.onclick = function () {
+ count = 0;
+ disp.innerHTML = count;
 }
