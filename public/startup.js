239c6c0ff2338db02d6b7e1d3129a56ee37b9ab4
Doddlebopper <122325330+Doddlebopper@users.noreply.github.com>
2023-04-05 20:01:26 -0600

    Update startup.js

--- public/startup.js
+++ public/startup.js
@@ -1,3 +1,22 @@
+function displayQuote(data) {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#quote');
+
+      const quoteEl = document.createElement('p');
+      quoteEl.classList.add('quote');
+      const authorEl = document.createElement('p');
+      authorEl.classList.add('author');
+
+      quoteEl.textContent = data.content;
+      authorEl.textContent = data.author;
+
+      containerEl.appendChild(quoteEl);
+      containerEl.appendChild(authorEl);
+    });
+}
+
 var count = 0;
 var starter = "";
 var btn = document.getElementById("btnCount");
@@ -67,23 +86,5 @@ function myFunction3() {
   document.getElementById("name3").value = "";
 }
 
-function displayQuote(data) {
-  fetch('https://api.quotable.io/random')
-    .then((response) => response.json())
-    .then((data) => {
-      const containerEl = document.querySelector('#quote');
-
-      const quoteEl = document.createElement('p');
-      quoteEl.classList.add('quote');
-      const authorEl = document.createElement('p');
-      authorEl.classList.add('author');
-
-      quoteEl.textContent = data.content;
-      authorEl.textContent = data.author;
-
-      containerEl.appendChild(quoteEl);
-      containerEl.appendChild(authorEl);
-    });
-}
 
 displayQuote();
